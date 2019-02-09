9cf7d8cecae91a511fca7c350efcb89afed9a1b2
Moosa Mohamed <moosa009692@gmail.com>
2019-02-09 15:48:10 +0530

    Updates

---
+++ precache-manifest.660eeb295c29ba1741378b2794be3e3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "ad01a16b50511d76e864",
+    "url": "/contactmanager/static/js/main.ad01a16b.chunk.js"
+  },
+  {
+    "revision": "e0d757ed43d04a78d9e9",
+    "url": "/contactmanager/static/js/1.e0d757ed.chunk.js"
+  },
+  {
+    "revision": "ad01a16b50511d76e864",
+    "url": "/contactmanager/static/css/main.5bf5a46e.chunk.css"
+  },
+  {
+    "revision": "e0d757ed43d04a78d9e9",
+    "url": "/contactmanager/static/css/1.4caf7f30.chunk.css"
+  },
+  {
+    "revision": "876210654a8e0462864cea7a416fba61",
+    "url": "/contactmanager/index.html"
+  }
+];
